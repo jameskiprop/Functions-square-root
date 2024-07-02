7b07bdb458278941875447cb710c5a7747a8adb2
jameskiprop <jameskiprop01@gmail.com>
2024-07-02 23:01:49 +0300

    map method to find a square root

--- root.js
+++ root.js
@@ -1,5 +1,12 @@
-let numbers = [4,9,16,25] 
+// Create a function that takes an array of numbers as an argument, and returns an array of the square roots of each of the numbers in the array. Print each of the returned square roots on the console in the following format:
 
-function squareRoot(numbers){
-    if( numbers)
-}
+// 4: 2
+// 9: 3
+// 16: 4
+
+// where the number on the left is an element in the array passed to the function, and the number on the right is its corresponding square root in the returned array.
+
+let nums = [4, 9, 16, 25];
+
+let sqrtNums = nums.map(Math.sqrt);
+console.log(sqrtNums);
