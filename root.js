8ced5f2a311a7bce73cca13345cd3f12892e35af
jameskiprop <jameskiprop01@gmail.com>
2024-07-06 22:07:16 +0300

    included a for loop method in the square root assignment method

--- root.js
+++ root.js
@@ -78,11 +78,31 @@ and returns an array of the square roots of those numbers that are greater than 
 So for instance if you have an array [4, 9, 16, 25, 36], and x is 3, 
 then the function should return [4, 5, 6].*/
 
-const numbers = [4, 9, 16, 25, 36, 49, 64, 81, 100];
-const x = 5;
+// function findSquareRoots(numbers, x) {
+//   return numbers
+//     .map((num) => Math.sqrt(num)) //Calculating the square roots of numbers
+//     .filter((sqrt) => sqrt > x); // filtering out to remain with the square roots of numbers above x
+// }
+// const numbers = [4, 9, 16, 25, 36, 49, 64, 81, 100];
+// const x = 5;
+
+// console.log(findSquareRoots(numbers, x));
+
+const numbers = [4, 9, 16, 25, 36, 49, 64, 81, 100]; //array initializaton
+const x = 6; //variable initialization
 function findSquareRoots(numbers, x) {
-  return numbers
-    .map((num) => Math.sqrt(num)) //compute the square root of a given number
-    .filter((sqrt) => sqrt > x); // give back the square root of numbers which are greater than x
+  if (x <= 5) {
+    console.log("Number should be  greater than 5");
+    return;
+  }
+  const squareRoots = []; // array to store the square roots
+
+  //looping through each number in the Numbers  array
+  for (let i = 0; i < numbers.length; i++) {
+    const sqrt = Math.sqrt(numbers[i]); //Calculating the square root
+    console.log(`${numbers[i]}: ${sqrt}`); //Print out the number and its square root
+    squareRoots.push(sqrt); //Add the square root of the array
+  }
+  return squareRoots; //Return the array of square roots
 }
 console.log(findSquareRoots(numbers, x));
